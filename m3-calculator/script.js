f1c2763cb8c12ba9829936e2375cebd97ffdf5cf
Muh Suryadi Triputra Rahman <msuryaditriputra@gmail.com>
2023-09-17 19:44:36 +0800

    add: id reset

--- m3-calculator/script.js
+++ m3-calculator/script.js
@@ -1,5 +1,6 @@
 const form = document.getElementById("form"),
     textarea = document.getElementById("textarea"),
+    reset = document.getElementById("reset"),
     result = document.getElementById("result"),
     mean = document.getElementById("mean"),
     median = document.getElementById("median"),
