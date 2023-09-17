20700a3c2adf37dba42869e3145f92d42c7e3be4
Muh Suryadi Triputra Rahman <msuryaditriputra@gmail.com>
2023-09-17 19:42:24 +0800

    selector

---
+++ m3-calculator/script.js
@@ -0,0 +1,6 @@
+const form = document.getElementById("form"),
+    textarea = document.getElementById("textarea"),
+    result = document.getElementById("result"),
+    mean = document.getElementById("mean"),
+    median = document.getElementById("median"),
+    mode = document.getElementById("mode");
